e1caf07929d421893eef9c7c0289e316eac6056f
Ashik M S <ashikms1998@gmail.com>
2024-02-20 01:39:39 +0530

    Time: 69 ms (47.53%) | Memory: 52.8 MB (5.86%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.js
+++ 2058-concatenation-of-array/concatenation-of-array.js
@@ -3,7 +3,7 @@
  * @return {number[]}
  */
 var getConcatenation = function(nums) {
-let first = [...nums]
-let newArr = nums.concat(nums)
+let first = nums
+let newArr = first.concat(first)
 return newArr
 };
