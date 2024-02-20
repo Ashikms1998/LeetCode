83b57781f3dcd79476fc1dd625bff061516ae99c
Ashik M S <ashikms1998@gmail.com>
2024-02-20 11:39:01 +0530

    Time: 142 ms (5.13%) | Memory: 57.5 MB (5.23%) - LeetSync

---
+++ 1791-richest-customer-wealth/richest-customer-wealth.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[][]} accounts
+ * @return {number}
+ */
+var maximumWealth = function(accounts) {
+    let array = [];
+    for (let i=0;i<accounts.length;i++){
+    let sum = 0;
+        for(j=0;j<accounts[i].length;j++){
+            sum =sum+accounts[i][j];
+            console.log(sum);
+        }
+        array.push(sum)
+    }
+        let temp = array[0];
+    for(let k=0;k<array.length;k++){
+        if(temp<array[k]){
+            temp =array[k]
+        }
+    }
+    return temp
+};
