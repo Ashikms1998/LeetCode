fac19c6b204c002e8bac50239623c360de8502f4
Ashik M S <ashikms1998@gmail.com>
2024-02-29 15:22:03 +0530

    Time: 50 ms (71.32%) | Memory: 48.4 MB (57.90%) - LeetSync

--- 66-plus-one/plus-one.js
+++ 66-plus-one/plus-one.js
@@ -4,8 +4,6 @@
  */
 var plusOne = function(digits) {
     let newOne = BigInt(digits.join(''))
-    console.log(newOne)
     let val = newOne+1n;
-    console.log(val)
     return val.toString().split('')
 };
