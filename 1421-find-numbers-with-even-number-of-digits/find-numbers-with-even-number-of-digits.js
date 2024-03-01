9c01ca713c37a544537cca6e6fc7ac9905ff8200
Ashik M S <ashikms1998@gmail.com>
2024-03-01 14:51:50 +0530

    Time: 62 ms (24.71%) | Memory: 50.7 MB (24.71%) - LeetSync

--- 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.js
+++ 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.js
@@ -5,9 +5,7 @@
 var findNumbers = function(nums) {
     let count = 0;
     for(i=0;i<nums.length;i++){
-        let current = nums[i].toString().length
-        console.log(current)
-        if(current%2==0){
+        if(nums[i].toString().length%2==0){
             count++;
         }
         
