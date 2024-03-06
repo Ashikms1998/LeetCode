0cb0576f6128574b12c6644ef2e5ffd42459a783
Ashik M S <ashikms1998@gmail.com>
2024-03-06 14:07:21 +0530

    Time: 141 ms (12.54%) | Memory: 58.8 MB (78.53%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.js
+++ 217-contains-duplicate/contains-duplicate.js
@@ -3,9 +3,9 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    nums.sort((a,b) => a-b);
-    for(let i = 0; i <= nums.length-1; i++){
-        if(nums[i] === nums[i+1]){
+    nums.sort((a,b)=>(a-b));
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]===nums[i+1]){
             return true
         }
     }
