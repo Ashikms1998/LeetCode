c0c2e72ea02bbbcbfbc966de658cb737ad71e2a4
Ashik M S <ashikms1998@gmail.com>
2024-03-20 23:15:45 +0530

    Time: 95 ms (5.16%) | Memory: 53.9 MB (14.22%) - LeetSync

---
+++ 258-add-digits/add-digits.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var addDigits = function(num) {
+    if(num<10){
+        return num
+    }else{
+        let first = Math.floor(num/10)
+        let second = Math.floor(num%10)
+        return addDigits(first+second)
+    }
+};
