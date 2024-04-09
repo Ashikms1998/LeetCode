67ebc96afc772f0490e70baf06acdac52048a93f
Ashik M S <ashikms1998@gmail.com>
2024-04-09 20:01:02 +0530

    Time: 64 ms (16.00%) | Memory: 49.1 MB (45.14%) - LeetSync

--- 2436-make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.js
+++ 2436-make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.js
@@ -7,8 +7,6 @@ var minimumOperations = function(nums) {
    const unique = new Set(nums)
    const uniquArr = Array.from(unique)
    let count=0;
-   console.log(uniquArr)
-
    for(let i=0;i<uniquArr.length;i++){
         if(uniquArr[i]!=0){
             count++;
