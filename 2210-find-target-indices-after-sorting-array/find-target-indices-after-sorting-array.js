1d718d82ab8ea94c13a51e90975e5d3574b2a731
Ashik M S <ashikms1998@gmail.com>
2024-04-14 15:10:03 +0530

    Time: 65 ms (19.93%) | Memory: 50.8 MB (22.64%) - LeetSync

---
+++ 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var targetIndices = function(nums, target) {
+    let arr = []
+    let sorted = nums.sort((a,b)=>(a-b));
+    for(let i=0;i<sorted.length;i++){
+        if(sorted[i]===target){
+            arr.push(i)
+        }
+    }
+    return arr;
+};
