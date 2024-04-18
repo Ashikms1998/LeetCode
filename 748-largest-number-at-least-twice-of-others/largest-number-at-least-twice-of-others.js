ef9b3ff0766be74804d5779121e777c22cf50a99
Ashik M S <ashikms1998@gmail.com>
2024-04-19 01:14:20 +0530

    Time: 60 ms (18.20%) | Memory: 50.4 MB (7.64%) - LeetSync

---
+++ 748-largest-number-at-least-twice-of-others/largest-number-at-least-twice-of-others.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var dominantIndex = function(nums) {
+    let nums1 =[]
+    for(let i=0;i<nums.length;i++){
+        nums1.push(nums[i])
+    }
+    let sorted = nums1.sort((a,b)=>(a-b));
+    if(sorted[nums.length-1]/2>=sorted[nums.length-2]){
+        for(let i=0;i<nums.length;i++){
+            if(nums[i]===sorted[nums.length-1]){
+                return i;
+            }
+        }
+    }else{
+        return -1
+    }
+};
