2c0910babbca689b3e4e1925830d09a7f10d719f
Ashik M S <ashikms1998@gmail.com>
2024-04-22 12:10:10 +0530

    Time: 54 ms (35.54%) | Memory: 48.2 MB (87.86%) - LeetSync

--- 434-number-of-segments-in-a-string/number-of-segments-in-a-string.js
+++ 434-number-of-segments-in-a-string/number-of-segments-in-a-string.js
@@ -5,12 +5,10 @@
 var countSegments = function(s) {
    let arr = [];
    let splitArr = s.split(" ")
-   console.log(splitArr)
    for(let i=0;i<splitArr.length;i++){
     if(splitArr[i].trim().length>=1){
         arr.push(splitArr[i])
     }
    }
-   console.log(arr)
    return arr.length
 };
