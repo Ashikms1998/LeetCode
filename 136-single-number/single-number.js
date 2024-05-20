27c1a37c9cb499139d42970ced9c6a45a8fe6a12
Ashik M S <ashikms1998@gmail.com>
2024-05-20 18:50:37 +0530

    Time: 584 ms (6.64%) | Memory: 50.1 MB (75.95%) - LeetSync

--- 136-single-number/single-number.js
+++ 136-single-number/single-number.js
@@ -3,10 +3,15 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
-    nums.sort((a,b)=>(a-b))
-    for(let i = 0;i<nums.length;i++){
-        if(nums[i]!=nums[i-1]&&nums[i]!=nums[i+1]){
-            return [nums[i]]
+    for(let i=0;i<nums.length;i++){
+        let count=0;
+        for(let j=0;j<nums.length;j++){
+            if(nums[i]===nums[j]){
+            count++
+            }
+        }
+        if(count==1){
+            return nums[i]
         }
     }
 };
