671e4e84f07e1a71ab8eda4c11d1c7c433256c8a
Ashik M S <ashikms1998@gmail.com>
2024-05-21 15:24:51 +0530

    Time: 59 ms (23.72%) | Memory: 50.1 MB (11.86%) - LeetSync

--- 748-largest-number-at-least-twice-of-others/largest-number-at-least-twice-of-others.js
+++ 748-largest-number-at-least-twice-of-others/largest-number-at-least-twice-of-others.js
@@ -3,14 +3,12 @@
  * @return {number}
  */
 var dominantIndex = function(nums) {
-    let nums1 =[...nums]
-    
-    let sorted = nums1.sort((a,b)=>(a-b));
-    if(sorted[nums.length-1]/2>=sorted[nums.length-2]){
-        for(let i=0;i<nums.length;i++){
-            if(nums[i]===sorted[nums.length-1]){
-                return i;
-            }
+    let deepCopy = [...nums];
+    let sorted = deepCopy.sort((a,b)=>(a-b))
+    if(sorted[sorted.length-1]/2>=sorted[sorted.length-2]){
+        for(let i=0;i<deepCopy.length;i++){
+            if(sorted[sorted.length-1]===nums[i])
+            return i
         }
     }else{
         return -1
