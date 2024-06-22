af5f9cc887788306d844e829f1e0feb1c5fc9acf
Ashik M S <ashikms1998@gmail.com>
2024-06-23 00:38:56 +0530

    Time: 60 ms (39.36%) | Memory: 49.6 MB (16.16%) - LeetSync

--- 520-detect-capital/detect-capital.js
+++ 520-detect-capital/detect-capital.js
@@ -3,22 +3,20 @@
  * @return {boolean}
  */
 var detectCapitalUse = function(word) {
-    let charArr = [];
-    let count = 0;
-    
-    for(let i = 0; i < word.length; i++){
-        charArr.push(word.charCodeAt(i));
+    let charArr = []
+    let count=0;
+    for(let i=0;i<word.length;i++){
+        charArr.push(word.charCodeAt(i))
+        
     }
-    
-    charArr.forEach((number) => {
-        if(number <= 90){
-            count++;
+    charArr.forEach((number)=>{
+        if(number<=90){
+            count++
         }
-    });
-    
-    if(count === word.length || count === 0 || (count === 1 && word.charCodeAt(0) <= 90)){
+    })
+    if(count===word.length||count===0|| (count===1&&word.charCodeAt(0)<=90)){
         return true;
-    } else {
+    }else{
         return false;
     }
 };
