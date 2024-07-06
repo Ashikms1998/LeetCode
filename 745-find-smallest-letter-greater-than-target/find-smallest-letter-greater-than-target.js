3a9cd42ee30994b1cf1a49859e19ea0025805800
Ashik M S <ashikms1998@gmail.com>
2024-07-06 23:54:08 +0530

    Time: 79 ms (5.43%) | Memory: 55.3 MB (6.07%) - LeetSync

---
+++ 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.js
@@ -0,0 +1,26 @@
+/**
+ * @param {character[]} letters
+ * @param {character} target
+ * @return {character}
+ */
+var nextGreatestLetter = function (letters, target) {
+    let arr = [];
+    let tarNum = target.charCodeAt()
+    for (let i = 0; i < letters.length; i++) {
+        if(letters[i].charCodeAt()>tarNum){
+        arr.push(letters[i].charCodeAt())
+        }
+    }
+    console.log(arr)
+    let deepSorted = JSON.parse(JSON.stringify(arr)).sort((a, b) => (a - b))
+    console.log(deepSorted[0], tarNum, "sadasd", String.fromCharCode(deepSorted[0]))
+    if(deepSorted[0]>tarNum&&deepSorted[0]!=tarNum){
+        return String.fromCharCode(deepSorted[0])
+    }else if(deepSorted[0]===tarNum){
+        return String.fromCharCode(deepsorted[i])
+
+    }else{
+        return letters[0]
+    }
+
+};
