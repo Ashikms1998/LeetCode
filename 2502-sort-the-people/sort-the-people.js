8679463dac320cb734b15dfd556260566a0381b9
Ashik M S <ashikms1998@gmail.com>
2024-07-22 11:18:41 +0530

    Time: 79 ms (42.15%) | Memory: 54.4 MB (71.30%) - LeetSync

--- 2502-sort-the-people/sort-the-people.js
+++ 2502-sort-the-people/sort-the-people.js
@@ -3,15 +3,18 @@
  * @param {number[]} heights
  * @return {string[]}
  */
-var sortPeople = function(names, heights) {
-    let arr = [];
-    for(let i =0;i<heights.length;i++){
+var sortPeople = function (names, heights) {
+
+    let arr =[];
+    for (let i = 0; i < names.length; i++) {
         arr.push([names[i],heights[i]])
     }
-    arr.sort((a,b)=>(b[1]-a[1]))
-    let newOne=[];
-    for(let i =0;i<arr.length;i++){
-        newOne.push(arr[i][0])
+    
+    let sorted = arr.sort((a,b)=>(b[1]-a[1]))
+    let sortedNames = [];
+    for(let i=0;i<names.length;i++){
+       sortedNames.push(sorted[i][0])
     }
-    return newOne;
+    return sortedNames
+    
 };
