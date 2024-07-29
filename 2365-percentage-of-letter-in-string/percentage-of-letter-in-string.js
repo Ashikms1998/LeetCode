9976363b66bfa3187f4bb6fed7644616bb7acd8f
Ashik M S <ashikms1998@gmail.com>
2024-07-29 15:29:49 +0530

    Time: 40 ms (98.29%) | Memory: 48.7 MB (64.57%) - LeetSync

--- 2365-percentage-of-letter-in-string/percentage-of-letter-in-string.js
+++ 2365-percentage-of-letter-in-string/percentage-of-letter-in-string.js
@@ -4,16 +4,13 @@
  * @return {number}
  */
 var percentageLetter = function(s, letter) {
-    let length = s.length;
     let count = 0;
     let splitted = s.split("")
-    console.log(splitted,count,length)
-    for(let i=0;i<length;i++){
+    for(let i=0;i<s.length;i++){
         if(splitted[i]=== letter){
             count++
         }
     }
-        console.log("ksksk",splitted,count,length)
 
-    return Math.floor((count/length)*100)
+    return Math.floor((count/s.length)*100)
 };
