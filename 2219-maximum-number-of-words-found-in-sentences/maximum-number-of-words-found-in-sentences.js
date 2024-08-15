921209408ef35263797617117b66222e0625d132
Ashik M S <ashikms1998@gmail.com>
2024-08-16 00:51:15 +0530

    Time: 63 ms (23.49%) | Memory: 51.2 MB (46.99%) - LeetSync

---
+++ 2219-maximum-number-of-words-found-in-sentences/maximum-number-of-words-found-in-sentences.js
@@ -0,0 +1,10 @@
+var mostWordsFound = function(sentences) {
+    let count = 0;
+    for(let i=0; i<sentences.length; i++){
+        let words = sentences[i].split(' ').length;
+        if(words > count){
+            count = words;
+        }
+    }
+    return count;
+};
