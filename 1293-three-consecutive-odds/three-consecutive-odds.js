baaa143b5599373dd731b4d6bee010e9d1df6328
Ashik M S <ashikms1998@gmail.com>
2024-08-17 11:25:25 +0530

    Time: 58 ms (23.48%) | Memory: 48.9 MB (53.97%) - LeetSync

--- 1293-three-consecutive-odds/three-consecutive-odds.js
+++ 1293-three-consecutive-odds/three-consecutive-odds.js
@@ -4,16 +4,10 @@
  */
 var threeConsecutiveOdds = function(arr) {
     let count =0;
-    for(let i=0;i<arr.length;i++){
-        if(arr[i]%2===1){
-            count++
-        }else{
-            count = 0
+    for(let i=0;i<arr.length-2;i++){
+        if(arr[i]%2===1&&arr[i+1]%2===1&&arr[i+2]%2===1){
+            return true
         }
-        if(count===3){
-            return true 
-        }
-
     }
     return false
 };
