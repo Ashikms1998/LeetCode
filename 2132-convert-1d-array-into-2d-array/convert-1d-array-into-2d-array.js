234b89af7a800cdfb2cb1495fbe9b3d4cff360d8
Ashik M S <ashikms1998@gmail.com>
2024-08-28 20:31:27 +0530

    Time: 166 ms (89.58%) | Memory: 69.1 MB (82.81%) - LeetSync

--- 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.js
+++ 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.js
@@ -4,19 +4,17 @@
  * @param {number} n
  * @return {number[][]}
  */
-var construct2DArray = function(original, m, n) {
-    let arr=[]
+var construct2DArray = function (original, m, n) {
+    let arr = []
     let splitLength = n;
-    if((m*n)===original.length&&splitLength<=original.length){
-        for(let i=0;i<n;i++){
-            for(let j=0;j<original.length;j++){
-                arr.push(original.slice(j,splitLength))
-                j= splitLength-1;
-                splitLength = splitLength+n;
-            }
-    return arr
+    if ((m * n) === original.length && splitLength <= original.length) {
+        for (let j = 0; j < original.length; j++) {
+            arr.push(original.slice(j, splitLength))
+            j = splitLength - 1;
+            splitLength = splitLength + n;
         }
-    }else{
+        return arr
+    } else {
         return []
     }
 
