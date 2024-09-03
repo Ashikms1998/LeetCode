66259cd6aba563d4f1de3269a434d6e99c3d475d
Ashik M S <ashikms1998@gmail.com>
2024-09-04 00:46:37 +0530

    Time: 57 ms (58.49%) | Memory: 49 MB (97.48%) - LeetSync

---
+++ 2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var getLucky = function (s, k) {
+    let sum = 0;
+    for (let i = 0; i < s.length; i++) {
+        let value = s.charCodeAt(i) - 96;
+        sum += Math.floor(value / 10) + (value % 10);
+    }
+    
+    for (let i = 1; i < k; i++) {
+        let nextSum = 0;
+        while (sum > 0) {
+            nextSum += sum % 10;
+            sum = Math.floor(sum / 10);
+        }
+        sum = nextSum;
+    }
+
+    return sum;
+};
