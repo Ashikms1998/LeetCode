e053c34651df5ad93a3a5402fbb2abdc6f893f73
Ashik M S <ashikms1998@gmail.com>
2024-09-25 12:33:53 +0530

    Time: 57 ms (33.23%) | Memory: 50.7 MB (28.57%) - LeetSync

--- 2274-keep-multiplying-found-values-by-two/keep-multiplying-found-values-by-two.js
+++ 2274-keep-multiplying-found-values-by-two/keep-multiplying-found-values-by-two.js
@@ -6,13 +6,10 @@
 var findFinalValue = function (nums, original) {
     let k = original
     nums = nums.sort((a, b) => (a - b))
-    console.log(nums,k)
         for (let i = 0; i < nums.length; i++) {
             if (nums[i] === k) {
                 k = k * 2
-                console.log(k,"1")
             }
         }
-        console.log(k,"2")
     return k
 };
