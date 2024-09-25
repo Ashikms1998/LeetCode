91ad0ef30d438b52d1a9f729cd6a6022d1fca59d
Ashik M S <ashikms1998@gmail.com>
2024-09-25 13:04:26 +0530

    Time: 51 ms (63.55%) | Memory: 48.1 MB (100.00%) - LeetSync

--- 2589-maximum-value-of-a-string-in-an-array/maximum-value-of-a-string-in-an-array.js
+++ 2589-maximum-value-of-a-string-in-an-array/maximum-value-of-a-string-in-an-array.js
@@ -6,13 +6,10 @@ var maximumValue = function (strs) {
     let arr = [];
     for (let i = 0; i < strs.length; i++) {
         if (Number(strs[i]) >= 0) {
-            arr.push(strs[i])
+            arr.push(Number(strs[i]))
         } else {
             arr.push(strs[i].length)
         }
-    }
-    for (let i = 0; i < arr.length; i++) {
-        arr[i] = Number(arr[i])
     }
     return Math.max(...arr)
 };
