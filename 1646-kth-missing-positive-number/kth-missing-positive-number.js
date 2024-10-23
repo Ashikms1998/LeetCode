1affc7801d50b7d0cdd0411775603fb5c2db35df
Ashik M S <ashikms1998@gmail.com>
2024-10-23 13:17:51 +0530

    Time: 31 ms (93.60%) | Memory: 50.6 MB (20.57%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.js
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.js
@@ -5,8 +5,8 @@
  */
 var findKthPositive = function (arr, k) {
     let brr = [];
-    let crr=[];
-    for (let i = 1; i <= arr[arr.length - 1]+k; i++) {
+    let crr = [];
+    for (let i = 1; i <= arr[arr.length - 1] + k; i++) {
         brr.push(i)
     }
     for (let i = 0; i < arr.length; i++) {
@@ -17,12 +17,12 @@ var findKthPositive = function (arr, k) {
         }
 
     }
-    for(let i=0;i<brr.length;i++){
-        if(brr[i]!=-1){
+    for (let i = 0; i < brr.length; i++) {
+        if (brr[i] != -1) {
             crr.push(brr[i])
         }
     }
-   return crr[k-1]
+    return crr[k - 1]
 
 
 };
