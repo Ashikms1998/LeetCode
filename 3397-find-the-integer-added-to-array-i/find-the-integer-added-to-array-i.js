22d62ba18bf352d16891cfe20979f3df2b5a96cc
Ashik M S <ashikms1998@gmail.com>
2024-11-05 16:38:27 +0530

    Time: 0 ms (100.00%) | Memory: 50.7 MB (72.22%) - LeetSync

--- 3397-find-the-integer-added-to-array-i/find-the-integer-added-to-array-i.js
+++ 3397-find-the-integer-added-to-array-i/find-the-integer-added-to-array-i.js
@@ -4,12 +4,8 @@
  * @return {number}
  */
 var addedInteger = function(nums1, nums2) {
-    let sum1= nums1.reduce((accumulator,currentVal)=>accumulator+currentVal);
-    let sum2= nums2.reduce((accumulator,currentVal)=>accumulator+currentVal);
-    let diff = Math.abs(sum2-sum1)/nums1.length
-    if(sum1>sum2){
-        return -1*diff
-    }else{
-        return diff
-    }
+    let valA = Math.min(...nums1)
+    let valB = Math.min(...nums2)
+    return valB-valA
+    
 };
