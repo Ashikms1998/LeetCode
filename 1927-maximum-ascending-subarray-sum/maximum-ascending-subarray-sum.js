72a8e73f3e423ca83bf16b4584f549ca284f1b39
Ashik M S <ashikms1998@gmail.com>
2025-02-04 11:51:05 +0530

    Time: 17 ms (7.14%) | Memory: 54 MB (5.95%) - LeetSync

---
+++ 1927-maximum-ascending-subarray-sum/maximum-ascending-subarray-sum.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxAscendingSum = function (nums) {
+    let arr = []
+    let sum = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] < nums[i + 1]) {
+            console.log(nums[i])
+            sum = sum + nums[i]
+        } else {
+            sum = sum + nums[i]
+            arr.push(sum)
+            sum = 0
+        }
+    }
+    return Math.max(...arr)
+};
